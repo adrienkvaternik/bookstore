092b01bd0e365ea072d23ab37883a312b7b89a48
Adrien Kvaternik <adrien.kvaternik@qualia.fr>
2016-08-29 19:03:27 +0200

    use Offer[] in OfferListDeserializer to get the offers list

--- app/src/main/java/fr/kvaternik/adrien/bookstore/model/service/deserializer/OfferListDeserializer.java
+++ app/src/main/java/fr/kvaternik/adrien/bookstore/model/service/deserializer/OfferListDeserializer.java
@@ -1,14 +1,11 @@
 package fr.kvaternik.adrien.bookstore.model.service.deserializer;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import fr.kvaternik.adrien.bookstore.model.entity.Offer;
@@ -19,15 +16,7 @@ import fr.kvaternik.adrien.bookstore.model.entity.Offer;
 public class OfferListDeserializer implements JsonDeserializer<List<Offer>> {
     @Override
     public List<Offer> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
-        JsonObject jsonObject = json.getAsJsonObject();
-        JsonArray jsonOffers = jsonObject.get("offers").getAsJsonArray();
-
-        List<Offer> offers = new ArrayList<>();
-        for (JsonElement jsonOffer : jsonOffers) {
-            Offer offer = context.deserialize(jsonOffer, Offer.class);
-            offers.add(offer);
-        }
-
-        return offers;
+        Offer[] offers = context.deserialize(json.getAsJsonObject().get("offers"), Offer[].class);
+        return Arrays.asList(offers);
     }
 }
